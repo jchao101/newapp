161954d56eecea08eb8cc1d2325406253cfd5473
jchao101 <jchao101@student.sscps.org>
2017-02-07 14:56:49 -0500

    2/7/17

--- app/src/main/java/com/example/techworkshop/newapp/MainActivity.java
+++ app/src/main/java/com/example/techworkshop/newapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.techworkshop.newapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
